d3224ce6876bf95144cd353b4be582b98d2aabd9
Dean Howe <hideanhowe@gmail.com>
2025-12-04 10:27:11 +0000

    test

--- index.js
+++ index.js
@@ -5,3 +5,22 @@
 // Every uppercase letter in the camel case string starts a new word.
 // Convert all letters to lowercase.
 // Separate words with an underscore (_).
+
+function toSnake(camel) {
+  // The input string will contain only letters (A-Z and a-z) and will always start with a lowercase letter.
+  // Every uppercase letter in the camel case string starts a new word.
+  // Convert all letters to lowercase.
+  // Separate words with an underscore (_).
+  console.log(camel);
+  const regex = /[A-Z]+/g;
+  const arr = camel.split(regex);
+
+  console.log(arr);
+  // let test = regex.test(camel)
+  // console.log(test)
+  return camel;
+}
+
+// The replace() method searches a string for a value or a regular expression.
+// The replace() method returns a new string with the value(s) replaced.
+// The replace() method does not change the original string.
